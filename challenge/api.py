556d0f971ba486f981791a289ccb8ef386aee811
Cristian Castaño <cristiancastano852@gmail.com>
2023-09-02 15:53:42 -0500

    create api and folder .github

--- challenge/api.py
+++ challenge/api.py
@@ -1,6 +1,20 @@
 import fastapi
+import pandas as pd
+from typing import List
+from model import DelayModel
+from pydantic import BaseModel
+from fastapi import HTTPException
 
 app = fastapi.FastAPI()
+
+# Define una clase Pydantic para los datos de vuelo
+class Flight(BaseModel):
+    OPERA: str
+    TIPOVUELO: str
+    MES: int
+
+class FlightData(BaseModel):
+    flights: List[Flight]
 
 @app.get("/health", status_code=200)
 async def get_health() -> dict:
@@ -9,5 +23,34 @@ async def get_health() -> dict:
     }
 
 @app.post("/predict", status_code=200)
-async def post_predict() -> dict:
-    return
+async def post_predict(codigo: FlightData) -> dict:
+        predictions = []
+        for flight in codigo.flights:
+            error_text = ''
+            if (flight.TIPOVUELO != 'N' and flight.TIPOVUELO != 'I') or (flight.MES < 1 or flight.MES > 12):
+                error_text=f"TIPOVUELO o MES no válido para el vuelo con OPERA {flight.OPERA}"
+                raise HTTPException(status_code=400, detail=error_text)
+            else:
+                df = pd.DataFrame([flight.dict()])
+                features = model.preprocess(
+                data=df
+                )
+                prediction = model.predict(features=features)
+                # predictions.append({"OPERA": flight.OPERA, "prediction": prediction})
+                predictions=prediction
+                return {"predict":  predictions}
+
+def initialize_model():
+    data = pd.read_csv(filepath_or_buffer="../data/data.csv")
+    model = DelayModel()
+    features, target = model.preprocess(
+        data=data,
+        target_column="delay"
+    )
+    model.fit(features, target)
+    return model
+
+if "__main__" == "__main__":
+    model = initialize_model()
+
+
--- challenge/model.py
+++ challenge/model.py
@@ -2,13 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import warnings
-from sklearn.metrics import accuracy_score
 from datetime import datetime
 import xgboost as xgb
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from sklearn.utils import shuffle
 from sklearn.metrics import confusion_matrix, classification_report
 from typing import Tuple, Union, List
 
@@ -25,6 +20,18 @@ class DelayModel:
         target_column: str = None,
         columns: str = None
     ) -> Union[Tuple[pd.DataFrame, pd.DataFrame], pd.DataFrame]:
+        """
+        Prepare raw data for training or predict.
+
+        Args:
+            data (pd.DataFrame): raw data.
+            target_column (str, optional): if set, the target is returned.
+
+        Returns:
+            Tuple[pd.DataFrame, pd.DataFrame]: features and target.
+            or
+            pd.DataFrame: features.
+        """
         top_10_features = [
             "OPERA_Latin American Wings", 
             "MES_7",
@@ -38,77 +45,62 @@ class DelayModel:
             "OPERA_Copa Air"
         ]
 
-        def get_period_day(date):
-            date_time = datetime.strptime(date, '%Y-%m-%d %H:%M:%S').time()
-            morning_min = datetime.strptime("05:00", '%H:%M').time()
-            morning_max = datetime.strptime("11:59", '%H:%M').time()
-            afternoon_min = datetime.strptime("12:00", '%H:%M').time()
-            afternoon_max = datetime.strptime("18:59", '%H:%M').time()
-            evening_min = datetime.strptime("19:00", '%H:%M').time()
-            evening_max = datetime.strptime("23:59", '%H:%M').time()
-            night_min = datetime.strptime("00:00", '%H:%M').time()
-            night_max = datetime.strptime("04:59", '%H:%M').time()
-
-            if(date_time > morning_min and date_time < morning_max):
-                return 'mañana'
-            elif(date_time > afternoon_min and date_time < afternoon_max):
-                return 'tarde'
-            elif(
-                (date_time > evening_min and date_time < evening_max) or
-                (date_time > night_min and date_time < night_max)
-            ):
-                return 'noche'
-
-        data['period_day'] = data['Fecha-I'].apply(get_period_day)
-
-        def is_high_season(fecha):
-            fecha_año = int(fecha.split('-')[0])
-            fecha = datetime.strptime(fecha, '%Y-%m-%d %H:%M:%S')
-            range1_min = datetime.strptime('15-Dec', '%d-%b').replace(year = fecha_año)
-            range1_max = datetime.strptime('31-Dec', '%d-%b').replace(year = fecha_año)
-            range2_min = datetime.strptime('1-Jan', '%d-%b').replace(year = fecha_año)
-            range2_max = datetime.strptime('3-Mar', '%d-%b').replace(year = fecha_año)
-            range3_min = datetime.strptime('15-Jul', '%d-%b').replace(year = fecha_año)
-            range3_max = datetime.strptime('31-Jul', '%d-%b').replace(year = fecha_año)
-            range4_min = datetime.strptime('11-Sep', '%d-%b').replace(year = fecha_año)
-            range4_max = datetime.strptime('30-Sep', '%d-%b').replace(year = fecha_año)
-            
-            if ((fecha >= range1_min and fecha <= range1_max) or 
-                (fecha >= range2_min and fecha <= range2_max) or 
-                (fecha >= range3_min and fecha <= range3_max) or
-                (fecha >= range4_min and fecha <= range4_max)):
-                return 1
-            else:
-                return 0
-            
-        data['high_season'] = data['Fecha-I'].apply(is_high_season)
-
-        def get_min_diff(data):
-            fecha_o = datetime.strptime(data['Fecha-O'], '%Y-%m-%d %H:%M:%S')
-            fecha_i = datetime.strptime(data['Fecha-I'], '%Y-%m-%d %H:%M:%S')
-            min_diff = ((fecha_o - fecha_i).total_seconds())/60
-            return min_diff
-        data['min_diff'] = data.apply(get_min_diff, axis = 1)
-
-        threshold_in_minutes = 15
-        data['delay'] = np.where(data['min_diff'] > threshold_in_minutes, 1, 0)
-
-        """
-        Prepare raw data for training or predict.
-
-        Args:
-            data (pd.DataFrame): raw data.
-            target_column (str, optional): if set, the target is returned.
-
-        Returns:
-            Tuple[pd.DataFrame, pd.DataFrame]: features and target.
-            or
-            pd.DataFrame: features.
-        """
-
-        # Separate features and target if 'target_column' is specified
         
         if target_column:
+            def get_period_day(date):
+                date_time = datetime.strptime(date, '%Y-%m-%d %H:%M:%S').time()
+                morning_min = datetime.strptime("05:00", '%H:%M').time()
+                morning_max = datetime.strptime("11:59", '%H:%M').time()
+                afternoon_min = datetime.strptime("12:00", '%H:%M').time()
+                afternoon_max = datetime.strptime("18:59", '%H:%M').time()
+                evening_min = datetime.strptime("19:00", '%H:%M').time()
+                evening_max = datetime.strptime("23:59", '%H:%M').time()
+                night_min = datetime.strptime("00:00", '%H:%M').time()
+                night_max = datetime.strptime("04:59", '%H:%M').time()
+
+                if(date_time > morning_min and date_time < morning_max):
+                    return 'mañana'
+                elif(date_time > afternoon_min and date_time < afternoon_max):
+                    return 'tarde'
+                elif(
+                    (date_time > evening_min and date_time < evening_max) or
+                    (date_time > night_min and date_time < night_max)
+                ):
+                    return 'noche'
+
+            data['period_day'] = data['Fecha-I'].apply(get_period_day)
+
+            def is_high_season(fecha):
+                fecha_año = int(fecha.split('-')[0])
+                fecha = datetime.strptime(fecha, '%Y-%m-%d %H:%M:%S')
+                range1_min = datetime.strptime('15-Dec', '%d-%b').replace(year = fecha_año)
+                range1_max = datetime.strptime('31-Dec', '%d-%b').replace(year = fecha_año)
+                range2_min = datetime.strptime('1-Jan', '%d-%b').replace(year = fecha_año)
+                range2_max = datetime.strptime('3-Mar', '%d-%b').replace(year = fecha_año)
+                range3_min = datetime.strptime('15-Jul', '%d-%b').replace(year = fecha_año)
+                range3_max = datetime.strptime('31-Jul', '%d-%b').replace(year = fecha_año)
+                range4_min = datetime.strptime('11-Sep', '%d-%b').replace(year = fecha_año)
+                range4_max = datetime.strptime('30-Sep', '%d-%b').replace(year = fecha_año)
+            
+                if ((fecha >= range1_min and fecha <= range1_max) or 
+                    (fecha >= range2_min and fecha <= range2_max) or 
+                    (fecha >= range3_min and fecha <= range3_max) or
+                    (fecha >= range4_min and fecha <= range4_max)):
+                    return 1
+                else:
+                    return 0
+            
+            data['high_season'] = data['Fecha-I'].apply(is_high_season)
+
+            def get_min_diff(data):
+                fecha_o = datetime.strptime(data['Fecha-O'], '%Y-%m-%d %H:%M:%S')
+                fecha_i = datetime.strptime(data['Fecha-I'], '%Y-%m-%d %H:%M:%S')
+                min_diff = ((fecha_o - fecha_i).total_seconds())/60
+                return min_diff
+            data['min_diff'] = data.apply(get_min_diff, axis = 1)
+
+            threshold_in_minutes = 15
+            data['delay'] = np.where(data['min_diff'] > threshold_in_minutes, 1, 0)
             features = data.drop(columns=[target_column])
             features = pd.concat([
                 pd.get_dummies(data['OPERA'], prefix = 'OPERA'),
@@ -120,14 +112,8 @@ class DelayModel:
             target = data[[target_column]]
             return features, target
         else:
-            features = data
-            features = pd.concat([
-                pd.get_dummies(data['OPERA'], prefix = 'OPERA'),
-                pd.get_dummies(data['TIPOVUELO'], prefix = 'TIPOVUELO'), 
-                pd.get_dummies(data['MES'], prefix = 'MES')], 
-                axis = 1
-            )
-            features= features[top_10_features]
+            features = pd.get_dummies(data, columns=["OPERA", "TIPOVUELO", "MES"])
+            features = features.reindex(columns=top_10_features, fill_value=0)
             return features
 
     def fit(
