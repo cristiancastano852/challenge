4aa32a58ffa029994eef26fd4f225236dd93d644
Cristian Castaño <cristiancastano852@gmail.com>
2023-09-02 16:35:26 -0500

    update test

--- tests/model/test_model.py
+++ tests/model/test_model.py
@@ -3,8 +3,7 @@ import pandas as pd
 from ..model import DelayModel
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
-from challenge.model import DelayModel
-from ...model import DelayModel
+from ..model import DelayModel
 
 class TestModel(unittest.TestCase):
 
