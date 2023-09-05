f7ca72ba45d284b71505f0275a4f471cb9751954
Cristian Castaño <cristiancastano852@gmail.com>
2023-09-05 01:54:54 -0500

    update api

--- challenge/api.py
+++ challenge/api.py
@@ -7,7 +7,6 @@ from fastapi import HTTPException
 
 app = fastapi.FastAPI()
 
-# Define una clase Pydantic para los datos de vuelo
 class Flight(BaseModel):
     OPERA: str
     TIPOVUELO: str
@@ -22,12 +21,13 @@ async def get_health() -> dict:
         "status": "OK"
     }
 
+#End point for predict the delay of a flight
 @app.post("/predict", status_code=200)
 async def post_predict(codigo: FlightData) -> dict:
         predictions = []
         for flight in codigo.flights:
             error_text = ''
-            if (flight.TIPOVUELO != 'N' and flight.TIPOVUELO != 'I') or (flight.MES < 1 or flight.MES > 12):
+            if (flight.TIPOVUELO not in ['N', 'I']) or (flight.MES < 1 or flight.MES > 12):
                 error_text=f"TIPOVUELO o MES no válido para el vuelo con OPERA {flight.OPERA}"
                 raise HTTPException(status_code=400, detail=error_text)
             else:
@@ -36,12 +36,11 @@ async def post_predict(codigo: FlightData) -> dict:
                 data=df
                 )
                 prediction = model.predict(features=features)
-                # predictions.append({"OPERA": flight.OPERA, "prediction": prediction})
                 predictions=prediction
                 return {"predict":  predictions}
 
 def initialize_model():
-    data = pd.read_csv(filepath_or_buffer="../data/data.csv")
+    data = pd.read_csv(filepath_or_buffer="./data/data.csv")
     model = DelayModel()
     features, target = model.preprocess(
         data=data,
